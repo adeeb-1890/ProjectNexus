fb7dac3b1e585c0aa6cf80387bd4a5f4dacedf16
Adeeb <adibulasar@gmail.com>
2026-01-31 12:52:30 +0530

    initial commit

---
+++ ProjectManagementSystem/src/test/java/com/Adeeb/ProjectManagementSystem/ProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Adeeb.ProjectManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
