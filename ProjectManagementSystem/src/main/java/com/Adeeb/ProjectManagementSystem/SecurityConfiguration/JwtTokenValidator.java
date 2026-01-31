83f5045aeabefbf9a7c4cf9d78f75e83a38f257d
Adeeb <adibulasar@gmail.com>
2026-01-31 14:30:03 +0530

    add JWT token validation filter

--- ProjectManagementSystem/src/main/java/com/Adeeb/ProjectManagementSystem/SecurityConfiguration/JwtTokenValidator.java
+++ ProjectManagementSystem/src/main/java/com/Adeeb/ProjectManagementSystem/SecurityConfiguration/JwtTokenValidator.java
@@ -1,4 +1,61 @@
 package com.Adeeb.ProjectManagementSystem.SecurityConfiguration;
 
-public class JwtTokenValidator {
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderThreadLocalAccessor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String SECRET_KEY =
+                "lkdhfalhfio54644haofajnfs545454ahahkfahkfhihaknfkaiufggo65445we4w4wqw5e5rrq5we5we5w5wew5";
+
+        String jwt = request.getHeader("Authorization");
+
+        if (jwt != null && jwt.startsWith("Bearer ")) {
+            jwt = jwt.substring(7);
+
+            try {
+                SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+                Claims claims = Jwts.parser()
+                        .verifyWith(key)
+                        .build()
+                        .parseSignedClaims(jwt)
+                        .getPayload();
+                String email = String.valueOf(claims.get("email"));
+                String authorities = String.valueOf(claims.get("authorities"));
+                List<GrantedAuthority> grantedAuthorities = AuthorityUtils.
+                        commaSeparatedStringToAuthorityList(authorities);
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(email , null , grantedAuthorities);
+                SecurityContextHolder.getContext().setAuthentication();
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
